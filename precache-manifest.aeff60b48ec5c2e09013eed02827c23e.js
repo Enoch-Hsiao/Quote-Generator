b92f74be9a0c4a01768d1f316a249cd391210e0d
Enoch Hsiao <51762139+MusicMaster-Code@users.noreply.github.com>
2020-01-15 11:38:56 -0500

    Updates

---
+++ precache-manifest.aeff60b48ec5c2e09013eed02827c23e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f6be3729ff6298031d1682ee103bfa",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "4063b40c2d1f9990cb10",
+    "url": "/quote-generator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f1c95c59794dbfe34409",
+    "url": "/quote-generator/static/js/2.61b88120.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quote-generator/static/js/2.61b88120.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4063b40c2d1f9990cb10",
+    "url": "/quote-generator/static/js/main.9ba538ce.chunk.js"
+  },
+  {
+    "revision": "f39db142146ffeee31ea",
+    "url": "/quote-generator/static/js/runtime-main.2369a586.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quote-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
